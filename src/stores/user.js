8f0b1862dcca66ae966aed3e140589c24bd7ae35
Ben <yu.shuming0829@gmail.com>
2022-07-27 11:08:35 +0800

    add stores/user.js

---
+++ src/stores/user.js
@@ -0,0 +1,23 @@
+import { defineStore } from "pinia";
+
+export default defineStore("User Store", {
+  // Data
+  state: () => ({
+    name: 'Benjamin',
+    wallet: '500',
+  }),
+
+  // Computed
+  // state 對應
+  getters: {
+    getUserNmae: (state) => `我的名字叫 ${ state.name }`,
+  },
+
+  // Methods
+  actions: {
+    // 可以使用 this
+    updateName() {
+      this.name = 'Tiffany';
+    },
+  }
+});
