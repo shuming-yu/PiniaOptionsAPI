d6a841ad057cd82fa90982f0dad4a964e0bc7313
Ben <yu.shuming0829@gmail.com>
2022-07-27 15:45:28 +0800

    path + reset

--- src/views/AboutComposition.vue
+++ src/views/AboutComposition.vue
@@ -13,9 +13,18 @@
 
   <button
     type="button"
-    @click="updateName"
-  >
+    @click="updateName">
     按我!!
+  </button>
+  <button
+    type="button"
+    @click="updateData">
+    updateData
+  </button>
+  <button
+    type="button"
+    @click="reset">
+    reset
   </button>
 </template>
 
@@ -27,22 +36,38 @@ import { storeToRefs } from 'pinia';
 export default {
   setup() {
     const user = userStore();
-    // reactive
+    // 方法一 : reactive 格式
     // user.name = '帥哥Ben';
-
-    // 解構形式取出
+    
+    // 方法二 : ref 形式, 解構形式取出
     const { name, wallet, getUserNmae } = storeToRefs(user); // 載入定義的 store
     console.log(name.value);
     name.value = 'Tommy';
 
+    // 方法三 : action 方式
     // 方法不需要雙向綁定, 可直接從 user 取出
     const { updateName } = user;
+
+    // 方法四 : path, 許多參數需要更新可使用 patch 方法
+    function updateData() {
+      user.$patch({
+        name: 'Nicky',
+        wallet: 80,
+      })
+    }
+    // 返回初始值 reset 方法
+    function reset() {
+      user.$reset();
+    }
+
     return {
       user,
       name,
       wallet,
       getUserNmae,
-      updateName
+      updateName,
+      updateData,
+      reset
     }
   }
 }
